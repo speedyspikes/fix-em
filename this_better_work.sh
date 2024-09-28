ef7a1a78d91a250353994383581b60d8906e4d06
speedyspikes <97717467+speedyspikes@users.noreply.github.com>
2024-09-28 04:12:49 -0600

    Update this_better_work.sh

--- this_better_work.sh
+++ this_better_work.sh
@@ -2,6 +2,9 @@
 arg=$1
 if [[ $arg = "?" ]]; then
     echo "ufw"
+    echo "ftp"
+    echo "ssh"
+    # echo "mysql"
 fi    
 
 # ufw
@@ -9,8 +12,8 @@ if [[ $arg = "" ]] || [[ $arg = "ufw" ]]; then
     sudo ufw default deny incoming
     sudo ufw default allow outgoing
     sudo ufw allow Apache
-    sudo ufw allow Apache Full
-    sudo ufw allow Apache Secure
+    sudo ufw allow 'Apache Full'
+    sudo ufw allow 'Apache Secure'
     sudo ufw allow CUPS
     sudo ufw allow OpenSSH
     sudo ufw allow Samba
@@ -18,3 +21,22 @@ if [[ $arg = "" ]] || [[ $arg = "ufw" ]]; then
     #sudo ufw allow from [IP]
     sudo ufw enable
 fi
+
+# ftp
+if [[ $arg = "" ]] || [[ $arg = "ftp" ]]; then
+    sudo service proftpd stop
+    sudo update-rc.d vsftpd disable
+fi
+
+# ssh
+if [[ $arg = "" ]] || [[ $arg = "ssh" ]]; then
+    sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.old
+    sudo cp sshd_config /etc/ssh/
+fi
+
+# mysql
+# if [[ $arg = "" ]] || [[ $arg = "mysql" ]]; then
+#     sudo mysql
+#     ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_strong_password';
+#     FLUSH PRIVILEGES;
+# fi

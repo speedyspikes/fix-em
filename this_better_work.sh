65ee35d77917df97c0eb96d52de3a338954c2479
speedyspikes <97717467+speedyspikes@users.noreply.github.com>
2024-09-28 10:52:29 -0600

    Fix firewall, etc

--- this_better_work.sh
+++ this_better_work.sh
@@ -2,32 +2,48 @@
 arg=$1
 if [[ $arg = "?" ]]; then
     echo "ufw"
-    # echo "ftp"
+    echo "passwd"
+    echo "ftp"
     echo "ssh"
     # echo "mysql"
-    echo "passwd"
 fi    
 
 # ufw
 if [[ $arg = "" ]] || [[ $arg = "ufw" ]]; then
     sudo ufw default deny incoming
     sudo ufw default allow outgoing
-    sudo ufw allow Apache
-    sudo ufw allow 'Apache Full'
-    sudo ufw allow 'Apache Secure'
+    #sudo ufw allow Apache
+    #sudo ufw allow 'Apache Full'
+    #sudo ufw allow 'Apache Secure'
     #sudo ufw allow CUPS
-    sudo ufw allow OpenSSH
-    sudo ufw allow Samba
+    #sudo ufw allow OpenSSH
+    #sudo ufw allow Samba
     #sudo ufw allow [port]
+    sudo ufw allow 21/tcp
+    sudo ufw allow 22/tcp
+    sudo ufw allow 80/tcp
+    sudo ufw allow 3500/tcp
+    sudo ufw allow 6697/tcp
     #sudo ufw allow from [IP]
     sudo ufw enable
 fi
 
+# passwd
+if [[ $arg = "" ]] || [[ $arg = "passwd" ]]; then
+    echo vagrant
+    sudo passwd
+    echo artoo_detoo
+    sudo passwd artoo_detoo
+    echo boba_fett
+    sudo passwd boba_fett
+fi
+
 # ftp
-# if [[ $arg = "" ]] || [[ $arg = "ftp" ]]; then
-#     sudo service proftpd stop
-#     sudo update-rc.d vsftpd disable
-# fi
+if [[ $arg = "" ]] || [[ $arg = "ftp" ]]; then
+     sudo service proftpd stop
+     sudo apt install vsftpd
+     sudo update-rc.d proftpd disable
+fi
 
 # ssh
 if [[ $arg = "" ]] || [[ $arg = "ssh" ]]; then
@@ -43,13 +59,3 @@ fi
 #     ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_strong_password';
 #     FLUSH PRIVILEGES;
 # fi
-
-# passwd
-if [[ $arg = "" ]] || [[ $arg = "passwd" ]]; then
-    echo vagrant
-    sudo passwd
-    echo artoo_detoo
-    sudo passwd artoo_detoo
-    echo boba_fett
-    sudo passwd boba_fett
-fi
